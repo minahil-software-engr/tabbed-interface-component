7267c513509d7646eb3b89f73d56e49eec0fc302
minahil-software-engr <engr.minahilrani@gmail.com>
2026-08-18 12:17:50 +0500

    Update removing elements.js

--- removing elements.js
+++ removing elements.js
@@ -1,17 +1,12 @@
 function showTab(index) {
     const container = document.getElementById('content-container');
-    
-    // 1. Pehle container ko khali karna (purana content remove karna)
+
     container.innerHTML = '';
-    
-    // 2. Templates mein se content uthana
     const templates = [
         document.getElementById('tab1-content'),
         document.getElementById('tab2-content'),
         document.getElementById('tab3-content')
     ];
-    
-    // 3. Jo tab click hua hai, uska content container ke andar insert karna
     const clone = templates[index].content.cloneNode(true);
     container.appendChild(clone);
 }
